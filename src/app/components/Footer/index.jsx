ec8dd8a08b13122798c4b61518b8f80bfb954551
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2025-04-19 19:57:50 +0200

    layout

---
+++ src/app/components/Footer/index.jsx
@@ -0,0 +1,3 @@
+export default function Footer() {
+  return <footer>Pirate Test Game Footer</footer>;
+}
--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,7 +1,15 @@
+import Footer from './components/Footer';
+import Header from './components/Header';
+import './globals.css';
+
 export default function RootLayout({ children }) {
   return (
     <html lang='en'>
-      <body>{children}</body>
+      <body className='flex flex-col min-h-screen'>
+        <Header />
+        <main className='grow'>{children}</main>
+        <Footer />
+      </body>
     </html>
   );
 }
